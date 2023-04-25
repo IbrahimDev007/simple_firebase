842e81941ec122f48a23b5a061511d0e978e6381
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-04-25 11:00:06 +0600

    git hub authintication adde

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import {
 	getAuth,
+	GithubAuthProvider,
 	GoogleAuthProvider,
 	onAuthStateChanged,
 	signInWithPopup,
 	signOut,
 } from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./app.css";
 import app from "./firebase/firebase.init";
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const GitProvider = new GithubAuthProvider();
 function App() {
+	const [loginUser, setloginUser] = useState(null);
+
 	const SignInWithGoogle = () => {
 		signInWithPopup(auth, provider)
-			.then((result) => console.log(result.user))
-			.then((error) => console.log(error));
+			.then((result) => setloginUser(result.user))
+			.catch((error) => console.log(error));
+	};
+	const SignInWithGithub = () => {
+		signInWithPopup(auth, GitProvider)
+			.then((result) => setloginUser(result.user))
+			.catch((error) => console.log(error));
+	};
+
+	const googleSignOut = () => {
+		signOut(auth).then(setloginUser(null));
 	};
 	useEffect(() => {
 		const unsubscribe = () => {
@@ -29,18 +42,21 @@ function App() {
 
 	return (
 		<div className="App">
-			This is Google Authintecation
+			This is firebase Authintecation practice
 			<br />
+			{loginUser && <h3>{loginUser.displayName}</h3>}
 			<br />
-			<button style={{ marginRight: "20px" }} onClick={SignInWithGoogle}>
+			<button className="mr-4 text-green-500" onClick={SignInWithGoogle}>
 				Google Sign In
 			</button>
-			<button
-				style={{ color: "red" }}
-				onClick={() => signOut(auth).then(console.log("signout"))}
-			>
-				Sign out
+			<button className="mr-4 text-yellow-400" onClick={SignInWithGithub}>
+				Github Sign In
 			</button>
+			{loginUser && (
+				<button className="text-red-600" onClick={googleSignOut}>
+					Sign out
+				</button>
+			)}
 		</div>
 	);
 }
